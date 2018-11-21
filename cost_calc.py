da5488ea71c5f7c2c52ce10b3abfeaf97c5a421b
Sam Lockart <samlockart@gmail.com>
2018-11-21 18:14:06 +1100

    Cleaning up line printing in cost_calc.py

--- cost_calc.py
+++ cost_calc.py
@@ -3,8 +3,8 @@
 import subprocess
 
 proc = subprocess.Popen('du -hs /storage/vault/ | awk \'{print substr($1, 1, length($1)-1)}\'', shell=True, stdout=subprocess.PIPE, )
-size = proc.communicate()[0]
+size = (proc.communicate()[0]).rstrip()
 price = 0.005 * float(size)
 print("Projected cost for " + str(size) + "GB is: $" + str(price))
-print("ZFS quota for /storage/vault is " + str(size) + "/500G")
+print("ZFS quota for /storage/vault is " + str(size) + "/500G\n")
 
