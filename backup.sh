78ccec48c3d1b8ea38f2d0b17c48c1aae1a93be5
Sam Lockart <samlockart@gmail.com>
2018-11-21 18:20:51 +1100

    Added some comments

--- backup.sh
+++ backup.sh
@@ -1,9 +1,9 @@
 #!/usr/local/bin/bash
-exec 1> >(logger -s -t $(basename $0)) 2>&1
+exec 1> >(logger -s -t $(basename $0)) 2>&1 # sends stdout/err to /var/log/messages
 # takes the variables SOURCE, DEST & EMAIL
 
 printf "Starting backup of $SOURCE to $DEST\n"
-if pgrep rclone > /dev/null
+if pgrep rclone > /dev/null # if rclone is already running
 then
 	printf 'Backup already running\n'
 	printf "Backup job already running on $HOST\n\n$(fortune)" | mail -s "$HOST: backup already in progress" $EMAIL
@@ -15,5 +15,5 @@ else
 	-v\
 	--transfers 32\
 	$SOURCE $DEST
-		printf "Backing up $HOST suceeded! \n\ndf -h:\n $(df -h)\nLast 32 lines of logging:\n$(tail -n 32 /tmp/rclone.log)" | mail -s "$HOST: backup success" $EMAIL
+		printf "$(./cost_calc.py)" | mail -s "$HOST: backup success" $EMAIL
 fi
