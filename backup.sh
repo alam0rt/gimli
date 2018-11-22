ab2f107a42f9719566e180ee50af4ad9b627ab50
Sam Lockart <sam@localhost.localdomain>
2018-11-22 12:17:25 +1100

    Converted the script to bash and created functions for primitive operations, etc

--- backup.sh
+++ backup.sh
@@ -2,18 +2,29 @@
 exec 1> >(logger -s -t $(basename $0)) 2>&1 # sends stdout/err to /var/log/messages
 # takes the variables SOURCE, DEST & EMAIL
 
-printf "Starting backup of $SOURCE to $DEST\n"
-if pgrep rclone > /dev/null # if rclone is already running
-then
-	printf 'Backup already running\n'
-	printf "Backup job already running on $HOST\n\n$(fortune)" | mail -s "$HOST: backup already in progress" $EMAIL
-	exit 0 
-else
-	printf "Starting back up of $HOST\n"
+
+perform_backup() {
+# this function simply performs the rclone sync and sends an email if rclone exits on a nonzero value
 	rclone sync\
 	--log-file /tmp/rclone.log\
 	-v\
 	--transfers 32\
 	$SOURCE $DEST
-		printf "$(./cost_calc.py)" | mail -s "$HOST: backup success" $EMAIL
+	rc=$?
+	if [[ $rc != 0 ]]; then send_mail_and_print "Backup failed with exit code $rc! $(tail -n 5 /tmp/rclone)" "Backup failed!"; exit $rc; fi
+
+send_mail_and_print() {
+# this function is similar to tee in that when passed 2 values it will send an email off as well as output to stdout (syslog)
+	printf "$1" | mail -s "$HOST: $2" $EMAIL
+	printf "$2" # only log subject to syslog
+}
+
+
+if pgrep rclone > /dev/null # if rclone is already running
+then
+	send_mail_and_print "Backup will not run backup again as rclone is already running\n" "Backup already running!"
+	exit 1
+else
+	perform_backup()
+	send_mail_and_print "$(./cost_calc.py)" "Backup has begun!"
 fi
